3920ede6eb18e9a70709e19d440c503cec28517e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-08-28 16:24:19 -0300

    Include vendor/aosp

--- aosp_kenzo.mk
+++ aosp_kenzo.mk
@@ -21,6 +21,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Provide meaningful APN configuration
 PRODUCT_COPY_FILES := device/xiaomi/kenzo/configs/apns-full-conf.xml:system/etc/apns-conf.xml
 
+# Vendor
+TARGET_ARCH := arm64
+$(call inherit-product-if-exists, vendor/aosp/config.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
